1d4544f69bf70b486d9314a8fa20b6f5baa8ec58
Dewang Agarwal <dk@lawn-128-61-20-245.lawn.gatech.edu>
2022-10-21 04:39:26 -0400

    Updated domains

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Select from "react-select";
 import { questions, choices, numberOfQuestions, arms } from "./constants.js";
 import "./App.css";
 import { Navbar, NavItem, Nav } from "react-bootstrap";
-axios.defaults.baseURL = 'https://sd-be.herokuapp.com';
+axios.defaults.baseURL = 'http://mental-health-sd.com';
 class App extends Component {
   constructor() {
     super();
